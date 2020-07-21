f02938825aa2815d8c901fcf81339211f9bebb67
Harvey <harvey.nguyen@outlook.com>
2020-07-21 18:26:06 +0200

    Open current page with outline in a new tab

--- background.js
+++ background.js
@@ -4,16 +4,8 @@
 
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+chrome.browserAction.onClicked.addListener(function(tab) {
+  var tabUrl = tab.url;
+  var outlineUrl = 'http://outline.com/' + tabUrl;
+  window.open(outlineUrl);
 });
